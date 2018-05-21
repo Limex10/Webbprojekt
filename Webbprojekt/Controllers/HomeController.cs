ca7178f1a90b50cc4e6ff0157474e6062e145cff
Emil Persson <9901peem@jonkoping.se>
2018-05-21 13:20:50 +0200

    Unfinished emailsending.

--- Webbprojekt/Controllers/HomeController.cs
+++ Webbprojekt/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Webbprojekt.Models;
+using System.Net;
+using System.Net.Mail;
+using MVCEmail.Models;
+
 
 namespace Webbprojekt.Controllers
 {
@@ -15,7 +19,45 @@ namespace Webbprojekt.Controllers
         {
             return View(db.Galleries.ToList());
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("recipient@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress("sender@outlook.com");  // replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
 
-
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "",
+                        Password = ""
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Sent()
+        {
+            return View();
+        }
     }
+
 }
