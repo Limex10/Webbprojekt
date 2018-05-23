3fcae17049ef921a3c7b537b1fd6452a465e68dc
Oscar Englund <oscare@live.se>
2018-05-23 23:33:49 +0200

    fixed layoooout

---
+++ Webbprojekt/Scripts/ourArtists.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $(".artist").mouseenter(function () {
+        $(this).find("#link-div").removeClass("link-div-inactive");
+        $(this).find("#link-div").addClass("link-div");
+
+        $(this).find("#artist-boka").removeClass("artist-boka-inactive");
+        $(this).find("#artist-boka").addClass("artist-boka");
+
+        $(this).find("img").css("filter", "blur(8px) grayscale(100%)");
+    });
+
+    $(".artist").mouseleave(function () {
+        $(this).find("#link-div").addClass("link-div-inactive");
+        $(this).find("#link-div").removeClass("link-div");
+
+        $(this).find("#artist-boka").addClass("artist-boka-inactive");
+        $(this).find("#artist-boka").removeClass("artist-boka");
+
+        $(this).find("img").css("filter", "none");
+    });
+});
